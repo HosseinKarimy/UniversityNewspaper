a669859c907ade7e2604cab0f1def62eb57bc167
hossein karimy <hosseinkm01@gmail.com>
2024-10-15 17:10:30 +0330

    clean up

--- Backend/API/Bazaar/EndPoints/AddBannerEndpoint.cs
+++ Backend/API/Bazaar/EndPoints/AddBannerEndpoint.cs
@@ -1,5 +1,5 @@
-﻿using Application.BazaarHandlers;
-using Application.DTO;
+﻿using Application.Bazaar.BazzarHandlers;
+using Application.Bazaar.DTO;
 using Carter;
 using Mapster;
 using MediatR;
@@ -14,11 +14,15 @@ public class AddBannerEndpoint : CarterModule
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapPost("/banners", async (AddBannerRequest request, IMediator mediator) =>
-        {
-            AddBannerCommand command = request.Adapt<AddBannerCommand>();
-            AddBannerResult result = await mediator.Send(command);
-            AddBannerResponse response = result.Adapt<AddBannerResponse>();
+        {  
+            //create Command
+            var command = new AddBannerCommand(request.BannerDto);
 
+            //Send Command to Mediator Pipeline
+            AddBannerResult result = await mediator.Send(command);
+
+            //Return response to client
+            AddBannerResponse response = result.Adapt<AddBannerResponse>();
             return Results.Ok(response.BannerId);
         });
     }
