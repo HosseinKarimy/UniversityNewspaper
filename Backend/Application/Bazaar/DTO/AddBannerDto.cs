2614dd6c2233a8f61e7acb949d0b77b801a34336
hossein karimy <hosseinkm01@gmail.com>
2024-10-16 11:21:55 +0330

    change class to record

--- Backend/Application/Bazaar/DTO/AddBannerDto.cs
+++ Backend/Application/Bazaar/DTO/AddBannerDto.cs
@@ -2,11 +2,10 @@
 
 namespace Application.Bazaar.DTO;
 
-public class AddBannerDto
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public Guid CategoryId { get; set; }
-    public string Image { get; set; }
-    public decimal Price { get; set; }
-}
+public record AddBannerDto(
+    string Title,
+    string Description,
+    Guid CategoryId,
+    string Image,
+    decimal Price
+);
