d16a3a80016934cf46de9ae089516004d0e01456
hossein karimy <hosseinkm01@gmail.com>
2025-02-15 14:59:15 +0330

    Fix Some Names

--- Backend/Helper/JsuServerResponse/ServerGetUserInfoResponse.cs
+++ Backend/Helper/JsuServerResponse/ServerGetUserInfoResponse.cs
@@ -1,6 +1,6 @@
 ﻿using Newtonsoft.Json;
 
-namespace Helper.ServerResponseDto;
+namespace Helper.JsuServerResponse;
 
 public class ServerGetUserInfoResponse
 {
--- Backend/Helper/JsuServerResponse/ServerUserInfo.cs
+++ Backend/Helper/JsuServerResponse/ServerUserInfo.cs
@@ -1,4 +1,4 @@
-﻿namespace Helper.ServerResponseDto;
+﻿namespace Helper.JsuServerResponse;
 
 public class ServerUserInfo
 {
