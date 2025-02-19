6c80973cb0cc2554ef27302f4f152463b32e6f8c
hossein karimy <hosseinkm01@gmail.com>
2025-02-19 16:16:18 +0330

    implement "GetEvents" Api

---
+++ Backend/API/Events/GetEventsEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Application.Events.EventsHandlers.GetEvents;
+using Carter;
+using Helper.JsuServerResponse;
+using MediatR;
+
+namespace API.Events;
+
+public class GetEventsEndpoint : CarterModule
+{
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Events", async (IMediator mediator) =>
+        {
+            var query = new GetEventsQuery();
+            var result = await mediator.Send(query);
+
+            return Results.Ok(JsuContractTemplate.GetContractTemplate("Success", result));
+        }).DisableAntiforgery();
+    }
+}
