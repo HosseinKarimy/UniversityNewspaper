35744f1475b6f0c4e6ff62226638fb3e2217d67b
hossein karimy <hosseinkm01@gmail.com>
2025-02-19 15:40:39 +0330

    modify paymentType prop of Event model as nullable

--- Backend/Domain/Models/RegistrationInfo.cs
+++ Backend/Domain/Models/RegistrationInfo.cs
@@ -8,5 +8,5 @@ public class RegistrationInfo
     public DateOnly? Deadline { get; set; }
     public int? Capacity { get; set; }
     public decimal? Fee { get; set; }
-    public PaymentType PaymentType { get; set; }
+    public PaymentType? PaymentType { get; set; }
 }
