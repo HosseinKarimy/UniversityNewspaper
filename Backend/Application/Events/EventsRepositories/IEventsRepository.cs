a0244e6bbf61696243213b7c210765230b9931cf
hossein karimy <hosseinkm01@gmail.com>
2025-03-15 16:39:23 +0330

    Fix Type of EventId in repositories

--- Backend/Application/Events/EventsRepositories/IEventsRepository.cs
+++ Backend/Application/Events/EventsRepositories/IEventsRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Application.Bazaar.BazzarRepositories;
 using Domain.Models;
+using Domain.StronglyTypes;
 
 namespace Application.Events.EventsRepositories
 {
-    public interface IEventsRepository : IRepository<Event,Guid>
+    public interface IEventsRepository : IRepository<Event,EventId>
     {
+
     }
 }
--- Backend/Infrastructure/Repositories/EventsRepositories/EventsRepositories.cs
+++ Backend/Infrastructure/Repositories/EventsRepositories/EventsRepositories.cs
@@ -1,9 +1,10 @@
 ﻿using Application.Events.EventsRepositories;
 using Domain.Models;
+using Domain.StronglyTypes;
 using Infrastructure.Data.ApplicaionDbContetxt;
 
 namespace Infrastructure.Repositories.EventsRepositories;
 
-public class EventsRepositories(AppDbContext dbContext) : Repository<Event, Guid>(dbContext.Events), IEventsRepository
+public class EventsRepositories(AppDbContext dbContext) : Repository<Event, EventId>(dbContext.Events), IEventsRepository
 {
 }
