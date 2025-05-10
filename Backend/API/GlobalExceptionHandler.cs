9a1e31d1daadc1f668a2eca91339a3156a3da3b8
hossein karimy <hosseinkm01@gmail.com>
2025-05-10 18:11:49 +0330

    Handle Custom Exceptions in GlobalExceptionHandler

--- Backend/API/GlobalExceptionHandler.cs
+++ Backend/API/GlobalExceptionHandler.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Exceptions;
+﻿using Application.Exceptions;
+using Domain.Exceptions;
 using Helper.JsuServerResponse;
 using Microsoft.AspNetCore.Diagnostics;
 
@@ -14,11 +15,31 @@ public class GlobalExceptionHandler : IExceptionHandler
             httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
             await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed", message: brv.Message), cancellationToken: cancellationToken);
             return true;
+        } else if (exception is NotFoundException nfe)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+            await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed", message: nfe.Message), cancellationToken: cancellationToken);
+            return true;
+        } else if (exception is UnauthorizedExeption ue)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed", message: ue.Message), cancellationToken: cancellationToken);
+            return true;
+        } else if (exception is AccessDeniedExcepion)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+            await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed", message: exception.Message), cancellationToken: cancellationToken);
+            return true;
+        } else if (exception is BadRequestException)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed", message: exception.Message), cancellationToken: cancellationToken);
+            return true;
         }
 
         // Fallback
         httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed" , message: exception.Message), cancellationToken: cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(JsuContractTemplate.GetContractTemplate("Failed", message: exception.Message), cancellationToken: cancellationToken);
         return true;
     }
 }
